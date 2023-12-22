7c79a23383ff88a1b6a9e493c3872cee5bd5f6ec
K S Prateek <ksprateek2004@gmail.com>
2023-12-22 22:45:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ def home():
     lon = 77.6070
     """Create a map object"""
     mapObj = folium.Map(location=[lat, lon],
-                        zoom_start=15)
+                        zoom_start=14)
 
     # add a marker to the map object
     folium.Marker([lat, lon],
                   popup="<i>This a marker</i>").add_to(mapObj)
 
     # set iframe width and height
-    mapObj.get_root().width = "2000px"
-    mapObj.get_root().height = "1000px"
+    mapObj.get_root().width = "100%"
+    mapObj.get_root().height = "1500px"
 
     # derive the iframe content to be rendered in the HTML body
     iframe = mapObj.get_root()._repr_html_()
@@ -28,11 +28,17 @@ def home():
         """
             <!DOCTYPE html>
             <html>
-                <head></head>
+                <head>
+                <style>
+                body{
+                    object-fit: fill;
+                }
+                </style>
+                </head>
                 <body>
-                    <h1>Using iframe to render folium map in HTML page</h1>
+                    <!--<h1>Using iframe to render folium map in HTML page</h1>-->
                     {{ iframe|safe }}
-                    <h3>This map is place in an iframe of the page!</h3>
+                    <!--<h3>This map is place in an iframe of the page!</h3>-->
                 </body>
             </html>
         """,
